ee9d83ec2211e15e484f01231a005952f66debc9
Max <162334963+Maxdgf@users.noreply.github.com>
2024-08-05 17:49:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-from tkinter import*
+from tkinter import *
 from random import randint
+from tkinter import messagebox
 
 root = Tk()
 root.title("ID maker")
-root.geometry("200x200")
-root.configure(bg = 'green')
+root.geometry("500x500")
+root.configure(bg="green")
+
 
 def exitRoot():
-	root.destroy()
-	
+    root.destroy()
+
+
 def generateID():
-	num1 = int(count1.get())
-	num2 = int(count2.get())
-	txt = "ID"
-	resultID = randint(num1, num2)
-	result = txt + str(resultID)
-	answer.insert(0, result)
-	
-#def copyID():
-	#app.clipboard_clear()
-	#selected = answer.get()
-	#app.clipboard_append(selected)
+    try:
+        num1 = int(count1.get())
+        num2 = int(count2.get())
+        txt = "ID"
+        resultID = randint(num1, num2)
+        result = txt + str(resultID)
+        answer.insert(0, result)
+    except:
+        messagebox.showerror("ERROR", "Number range is null!")
+
+
+# def copyID():
+# app.clipboard_clear()
+# selected = answer.get()
+# app.clipboard_append(selected)
 
 Label(text="ID maker", font=("Verdana", 30)).pack(pady=50)
 Label(text="Number range:").pack(pady=10)
@@ -32,8 +39,8 @@ Label(text="Result:").pack(pady=10)
 answer = Listbox(width=50, height=1)
 answer.pack()
 Button(text="Generate ID", bg="yellow", command=generateID).pack(pady=20)
-#Label(text="Copy ID:").pack(pady=20)
-#Button(text="Copy ID", bg="orange", command=copyID).pack()
+# Label(text="Copy ID:").pack(pady=20)
+# Button(text="Copy ID", bg="orange", command=copyID).pack()
 Button(text="exit", bg="red", width=3, command=exitRoot).place(x=0, y=0)
 
-root.mainloop
+root.mainloop()
